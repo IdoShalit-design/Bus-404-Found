de7361c9cb8d21db17eb4666423af0c1a974d3fa
IdoShalit-design <idoshalit@gmail.com>
2026-03-01 21:53:58 +0200

    Revert "Fix WiFi scan memory leak in NetworkManager::print_networks() (#8)"
    This reverts commit 98e8b15b77b7c966ea33c6f3cfd39d1d2e52b913.

--- src/Network/NetworkManager.cpp
+++ src/Network/NetworkManager.cpp
@@ -49,8 +49,6 @@ void NetworkManager::print_networks() {
         Serial.println(WiFi.channel(i));
     }
     Serial.println("-----------------------");
-
-    WiFi.scanDelete();
 }
 
 void NetworkManager::print_wifi_status() {
